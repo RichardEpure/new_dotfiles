99e966058e4fe4cc845a53f06e9d78db04f4dc48
Richard <richardvandick@gmail.com>
2023-09-29 22:28:54 +0100

    load colours only if in nvim

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,9 +1,13 @@
+local is_neovim = require("functions").is_neovim
+
 function SetColor(color)
     vim.cmd.colorscheme(color)
 end
 
-vim.g.gruvbox_material_background = "hard"
-vim.g.gruvbox_material_foreground = "material"
-vim.g.gruvbox_material_transparent_background = 0
+if is_neovim() then
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_foreground = "material"
+    vim.g.gruvbox_material_transparent_background = 0
 
-SetColor("gruvbox-material")
+    SetColor("gruvbox-material")
+end
