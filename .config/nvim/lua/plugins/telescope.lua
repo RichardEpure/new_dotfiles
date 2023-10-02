9f07cd947012341503a500fecff97a54362576f3
Richard <richardvandick@gmail.com>
2023-10-02 14:44:50 +0100

    add telescope maps for git

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,8 +8,10 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fa', builtin.find_files)
+        vim.keymap.set('n', '<leader>ff', builtin.git_files)
+        vim.keymap.set('n', '<leader>fw', builtin.live_grep)
+        vim.keymap.set('n', '<leader>fc', builtin.git_commits)
+        vim.keymap.set('n', '<leader>fb', builtin.git_branches)
     end
 }
