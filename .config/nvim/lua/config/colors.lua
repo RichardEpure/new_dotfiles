5455b8b0f3dac4782413b3dbc1f596a8176b2909
Richard <richardvandick@gmail.com>
2023-10-10 13:26:48 +0100

    remove rainbow

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,5 +1,4 @@
 local is_neovim = require("config.utils").is_neovim
-local palette = require("config.palette")
 
 function SetColor(color)
     vim.cmd.colorscheme(color)
@@ -11,12 +10,5 @@ if is_neovim() then
     vim.g.gruvbox_material_transparent_background = 0
 
     SetColor("gruvbox-material")
-
-    vim.api.nvim_set_hl(0, 'RainbowDelimiterRed', { fg = palette.red })
-    vim.api.nvim_set_hl(0, 'RainbowDelimiterYellow', { fg = palette.yellow })
-    vim.api.nvim_set_hl(0, 'RainbowDelimiterGreen', { fg = palette.green })
-    vim.api.nvim_set_hl(0, 'RainbowDelimiterBlue', { fg = palette.blue })
-    vim.api.nvim_set_hl(0, 'RainbowDelimiterOrange', { fg = palette.orange })
-    vim.api.nvim_set_hl(0, 'RainbowDelimiterViolet', { fg = palette.purple })
 end
 
