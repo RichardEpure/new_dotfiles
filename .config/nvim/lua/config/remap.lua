bffb940f3d5d8edd81545f943656c4d5d1ebda21
Richard <richardvandick@gmail.com>
2023-10-15 23:27:17 +0100

    change insert mode paste remap

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -191,6 +191,6 @@ else
         vim.keymap.set({ "n", "v" }, "<C-c>", '"+y', { desc = "Copy to clipboard" })
         vim.keymap.set({ "n", "v" }, "<C-x>", '"+x', { desc = "Cut to clipboard" })
         vim.keymap.set({ "n", "v" }, "<C-v>", '"+gP', { desc = "Paste from clipboard" })
-        vim.keymap.set({ "i", 't' }, "<C-v>", '<esc>"+gP', { desc = "Paste from clipboard" })
+        vim.keymap.set({ "i", 't' }, "<C-v>", '<esc>"+gp', { desc = "Paste from clipboard" })
     end
 end
