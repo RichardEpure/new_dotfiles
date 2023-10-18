0e834b91a1c048119c2d55268ffafb4b01462b6b
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-18 15:51:51 +0100

    add incremental selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -58,6 +58,16 @@ return {
                 enable = true
             },
 
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = '<CR>',
+                    scope_incremental = '<CR>',
+                    node_incremental = '<TAB>',
+                    node_decremental = '<S-TAB>',
+                },
+            },
+
             autotag = {
                 enable = true,
             },
