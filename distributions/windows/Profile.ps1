fc7df627f5a5dd4eb02a8c3ac9215038998cea5c
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-22 16:08:59 +0100

    update ps profile

--- distributions/windows/Profile.ps1
+++ distributions/windows/Profile.ps1
@@ -2,6 +2,40 @@ $logPath = "$env:USERPROFILE/Profile.log"
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 "`n$($stopwatch.ElapsedMilliseconds)ms`tProfile load started" | Out-File -FilePath $logPath -Append
 
+# Aliases
+Set-Alias -Name sa -Value Start-AdminSession
+Set-Alias -Name cdf -Value Set-Directory-Fuzzy
+
+"$($stopwatch.ElapsedMilliseconds)ms`tAliases set" | Out-File -FilePath $logPath -Append
+
+# Functions
+function Set-Directory-Fuzzy {
+    <#
+    .SYNOPSIS
+        Navigates to a file in the current directory and all subdirectories.
+    #>
+    param (
+        # The drive to navigate to before searching.
+        [string]$DriveLetter
+    )
+
+    if ($DriveLetter) {
+        Set-Location $DriveLetter":/"
+    } 
+
+    Get-ChildItem . -Recurse | Where-Object { $_.PSIsContainer } | Invoke-Fzf | Set-Location
+}
+
+function Start-AdminSession {
+    <#
+    .SYNOPSIS
+        Starts a new PowerShell session with elevated rights. Alias: su 
+    #>
+    Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command &{Set-Location $PWD}"
+}
+
+"$($stopwatch.ElapsedMilliseconds)ms`tFunctions loaded" | Out-File -FilePath $logPath -Append
+
 # Promp Setup
 Invoke-Expression (&starship init powershell)
 
