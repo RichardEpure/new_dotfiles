791b9aaefff982efd370595be624a748a46b4fb4
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-23 19:42:18 +0100

    add telescope binding for show hidden

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,5 +11,6 @@ return {
         vim.keymap.set('n', '<leader>fa', builtin.find_files)
         vim.keymap.set('n', '<leader>ff', builtin.git_files)
         vim.keymap.set('n', '<leader>fw', builtin.live_grep)
+        vim.keymap.set('n', '<leader>fh', function() builtin.find_files({ hidden = true }) end)
     end
 }
