71c3ff1322331b01b0b6dd4624462970886f6aa2
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-23 19:54:38 +0100

    use telescope to change directory

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,6 +6,24 @@ return {
     version = '*',
     enabled = is_neovim,
     config = function()
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    mappings = {
+                        n = {
+                            ["cd"] = function(prompt_bufnr)
+                                local selection = require("telescope.actions.state").get_selected_entry()
+                                local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+                                require("telescope.actions").close(prompt_bufnr)
+                                -- Depending on what you want put `cd`, `lcd`, `tcd`
+                                vim.cmd(string.format("silent cd %s", dir))
+                            end
+                        }
+                    }
+                },
+            }
+        })
+
         local builtin = require('telescope.builtin')
 
         -- We cache the results of "git rev-parse"
