19a84c72a4ade864ac57d71de7152f85098ddcc7
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-23 19:42:29 +0100

    fallback to all files if not a git repo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,8 +8,27 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
 
+        -- We cache the results of "git rev-parse"
+        -- Process creation is expensive in Windows, so this reduces latency
+        local is_inside_work_tree = {}
+        local project_files = function()
+            local opts = {} -- define here if you want to define something
+
+            local cwd = vim.fn.getcwd()
+            if is_inside_work_tree[cwd] == nil then
+                vim.fn.system("git rev-parse --is-inside-work-tree")
+                is_inside_work_tree[cwd] = vim.v.shell_error == 0
+            end
+
+            if is_inside_work_tree[cwd] then
+                builtin.git_files(opts)
+            else
+                builtin.find_files(opts)
+            end
+        end
+
         vim.keymap.set('n', '<leader>fa', builtin.find_files)
-        vim.keymap.set('n', '<leader>ff', builtin.git_files)
+        vim.keymap.set('n', '<leader>ff', project_files)
         vim.keymap.set('n', '<leader>fw', builtin.live_grep)
         vim.keymap.set('n', '<leader>fh', function() builtin.find_files({ hidden = true }) end)
     end
