b0ea0b1c672e300ba8e02edc31f328c1e161579f
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-24 20:07:56 +0100

    add formatting commands and change mapping, also lsp fallback

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -18,10 +18,47 @@ return {
 
                 gdscript = { 'gdformat' }
             },
+            format_on_save = function(bufnr)
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_fallback = true }
+            end,
         })
 
-        vim.keymap.set({ 'n', 'x' }, '<leader>jj', function()
-            require('conform').format()
-        end)
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+            if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, { desc = "Disable autoformat-on-save", bang = true, })
+
+        vim.api.nvim_create_user_command("FormatEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, { desc = "Re-enable autoformat-on-save", })
+
+        vim.api.nvim_create_user_command("FormatToggle", function()
+            vim.b.disable_autoformat = not vim.b.disable_autoformat
+            vim.g.disable_autoformat = not vim.g.disable_autoformat
+        end, { desc = "Toggle autoformat-on-save", })
+
+        vim.api.nvim_create_user_command("Format", function(args)
+            local range = nil
+            if args.count ~= -1 then
+                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                range = {
+                    start = { args.line1, 0 },
+                    ["end"] = { args.line2, end_line:len() },
+                }
+            end
+            require("conform").format({ async = true, lsp_fallback = true, range = range })
+        end, { range = true })
+
+        vim.keymap.set({ 'n', 'x' }, '<C-f>', [[Format<CR>]])
     end
 }
+
