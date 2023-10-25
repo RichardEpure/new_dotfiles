778bbd158c0f42915731c8b5346cc14e3f6a6334
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-25 20:22:22 +0100

    add mapping for searching recently opened files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -47,7 +47,9 @@ return {
 
         vim.keymap.set('n', '<leader>fa', builtin.find_files)
         vim.keymap.set('n', '<leader>ff', project_files)
+        vim.keymap.set('n', '<leader>fo', builtin.oldfiles)
         vim.keymap.set('n', '<leader>fw', builtin.live_grep)
         vim.keymap.set('n', '<leader>fh', function() builtin.find_files({ hidden = true }) end)
     end
 }
+
