60ec494153aa2c7c4690b6825d5f444a337064d9
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-10-29 13:18:59 +0000

    update install script, add .profile & zoxide

--- distributions/ubuntu/setup.sh
+++ distributions/ubuntu/setup.sh
@@ -15,6 +15,26 @@ if [ -f ~/.gitconfig ]; then
   rm ~/.gitconfig
 fi
 
+if [ -f ~/.profile ]; then
+  echo "Removing ~/.profile"
+  rm ~/.profile
+fi
+
 echo "Creating symbolic links..."
 ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/distributions/ubuntu/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/distributions/ubuntu/.profile ~/.profile
+
+# Install starship
+if ! command -v starship &> /dev/null
+then
+  echo "Installing starship..."
+  curl -sS https://starship.rs/install.sh | sh
+fi
+
+# Install zoxide
+if ! command -v zoxide &> /dev/null
+then
+  echo "Installing zoxide..."
+  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+fi
