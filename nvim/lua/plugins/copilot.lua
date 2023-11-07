7554e9e9b5eb39c54d4e6d83742f46e2224ddc5c
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-11-07 23:06:11 +0000

    change mappings for copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -27,12 +27,12 @@ return {
                 auto_trigger = true,
                 debounce = 75,
                 keymap = {
-                    accept = "<M-l>",
+                    accept = "<C-a>",
                     accept_word = false,
                     accept_line = false,
                     next = "<M-]>",
                     prev = "<M-[>",
-                    dismiss = "<C-]>",
+                    dismiss = "<C-e>",
                 },
             },
             filetypes = {
