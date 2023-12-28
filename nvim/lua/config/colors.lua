8571fb6024d8913049245fdf44959c623928ac36
Richard <43828146+RichardEpure@users.noreply.github.com>
2023-12-28 21:08:34 +0000

    set highlights depending on theme and background

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -5,12 +5,16 @@ local M = {}
 local setColour = function(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#585858' })
+    if (vim.g.colors_name == "gruvbox-material" and vim.g.gruvbox_material_background == "hard")
+        or vim.g.colors_name == "alabaster" then
+        vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#444444' })
+    end
 end
 
 M.setColour = setColour
 
 if is_neovim() then
-    vim.g.gruvbox_material_background = "medium"
+    vim.g.gruvbox_material_background = "hard"
     vim.g.gruvbox_material_foreground = "material"
     vim.g.gruvbox_material_transparent_background = 0
 
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -7,10 +7,11 @@ return {
     config = function()
         local hooks = require("ibl.hooks")
         hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#353535' })
             if vim.g.colors_name == "alabaster" then
                 vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#1b2628' })
-            else
-                vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#353535' })
+            elseif vim.g.colors_name == "gruvbox-material" and vim.g.gruvbox_material_background == "hard" then
+                vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#2a2a2a' })
             end
         end)
 
