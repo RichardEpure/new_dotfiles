cef2559b86ff9e4cc8d605dd43580df0b6f50c2b
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-03 18:30:32 +0000

    set dir with yazi & add scoop packages

--- distributions/windows/Profile.ps1
+++ distributions/windows/Profile.ps1
@@ -5,6 +5,7 @@ $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 # Aliases
 Set-Alias -Name sa -Value Start-AdminSession
 Set-Alias -Name cdf -Value Set-DirectoryFuzzy
+Set-Alias -Name ya -Value Open-Yazi
 
 "$($stopwatch.ElapsedMilliseconds)ms`tAliases set" | Out-File -FilePath $logPath -Append
 
@@ -32,6 +33,20 @@ function Start-AdminSession {
         Starts a new PowerShell session with elevated rights.
     #>
     Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command &{Set-Location $PWD}"
+}
+
+function Open-Yazi {
+    <#
+    .SYNOPSIS
+        Opens yazi at your current directory. cd into the last directory in yazi when you exit.
+    #>
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -Path $cwd
+    }
+    Remove-Item -Path $tmp
 }
 
 "$($stopwatch.ElapsedMilliseconds)ms`tFunctions loaded" | Out-File -FilePath $logPath -Append
--- distributions/windows/Setup.ps1
+++ distributions/windows/Setup.ps1
@@ -56,6 +56,9 @@ $chocoDependencies = @(
 # Scoop dependencies
 $scoopDependencies = @(
     "yazi"
+    "neovide"
+    "ninja"
+    "7zip"
 )
 
 # Required PowerShell Modules
