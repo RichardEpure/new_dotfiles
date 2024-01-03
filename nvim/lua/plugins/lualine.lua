eda9968f6cfcbfef3edcafef3645d801ce0c0daf
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-03 21:44:00 +0000

    remove modes & add relative path to status bar

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -27,9 +27,14 @@ return {
                 }
             },
             sections = {
-                lualine_a = { 'mode' },
+                lualine_a = {},
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { 'filename' },
+                lualine_c = {
+                    {
+                        'filename',
+                        path = 1
+                    }
+                },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
