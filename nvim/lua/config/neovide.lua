53a1f26e65f8681302241189f5125dd74e35cf62
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-04 23:09:51 +0000

    add command for toggling neovide scroll animation

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -8,5 +8,19 @@ if vim.g.neovide then
     vim.g.neovide_refresh_rate_idle = 120
     vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_scroll_animation_length = 0.05
+    vim.api.nvim_create_user_command("AnimationToggleNeovideScroll",
+        function()
+            local val = vim.g.neovide_scroll_animation_length
+            print(val)
+            if val == 0 then
+                vim.g.neovide_scroll_animation_length = 0.05
+                print("Scroll animation enabled")
+            else
+                vim.g.neovide_scroll_animation_length = 0
+                print("Scroll animation disabled")
+            end
+        end,
+        { nargs = 0, }
+    )
 end
 
