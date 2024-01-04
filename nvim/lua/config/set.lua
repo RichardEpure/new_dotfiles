c42378974a3e43f6d230b580384aa3d091f8537a
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-04 22:27:18 +0000

    enable folds

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -10,7 +10,7 @@ if vim.g.vscode == nil then
     vim.o.foldcolumn = "1"
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
-    vim.o.foldenable = false
+    vim.o.foldenable = true
 
     vim.opt.termguicolors = true
     vim.opt.nu = true
