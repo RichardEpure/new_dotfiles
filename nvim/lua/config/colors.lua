82d5fd060dd1f6111bcbbde6d330bb2ef09ad741
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-05 17:35:27 +0000

    move colour related stuff into colors.lua

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -1,6 +1,6 @@
 local is_neovim = require("config.utils").is_neovim
-
-local M = {}
+local actions = require("telescope.actions")
+local actions_state = require("telescope.actions.state")
 
 local setColour = function(color)
     vim.cmd.colorscheme(color)
@@ -11,15 +11,29 @@ local setColour = function(color)
     end
 end
 
-M.setColour = setColour
+local setColourTelescope = function(prompt_bufnr)
+    local selected = actions_state.get_selected_entry()
+    setColour(selected.value)
+    actions.close(prompt_bufnr)
+end
 
 if is_neovim() then
+    vim.api.nvim_create_user_command("Colour",
+        function()
+            require("telescope.builtin").colorscheme({
+                attach_mappings = function(_, map)
+                    map("i", "<CR>", setColourTelescope)
+                    map("n", "<CR>", setColourTelescope)
+                    return true
+                end,
+            })
+        end,
+        { nargs = 0, }
+    )
+
     vim.g.gruvbox_material_background = "hard"
     vim.g.gruvbox_material_foreground = "material"
     vim.g.gruvbox_material_transparent_background = 0
 
     setColour("gruvbox-material")
 end
-
-return M
-
