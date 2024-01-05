2590ce85aa9cf5e7682708c5011acd4577cf5992
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-05 18:11:21 +0000

    edit gruvbox floating windows highlights

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -5,8 +5,15 @@ local actions_state = require("telescope.actions.state")
 local setColour = function(color)
     vim.cmd.colorscheme(color)
     vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#585858' })
-    if (vim.g.colors_name == "gruvbox-material" and vim.g.gruvbox_material_background == "hard")
-        or vim.g.colors_name == "alabaster" then
+
+    if vim.g.colors_name == "gruvbox-material" then
+        vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
+        vim.api.nvim_set_hl(0, 'FloatTitle', { link = 'Orange' })
+        vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'TelescopeBorder' })
+    end
+
+    if (vim.g.colors_name == "gruvbox-material" and vim.g.gruvbox_material_background == "hard") or
+        vim.g.colors_name == "alabaster" then
         vim.api.nvim_set_hl(0, 'MiniIndentscopeSymbol', { fg = '#444444' })
     end
 end
