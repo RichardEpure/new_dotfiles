7e334c4aa95c394d05023b6d329f1e52b96b1345
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-11 22:13:25 +0000

    small lualine tweaks

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -19,7 +19,7 @@ return {
                 },
                 ignore_focus = {},
                 always_divide_middle = true,
-                globalstatus = false,
+                globalstatus = true,
                 refresh = {
                     statusline = 1000,
                     tabline = 1000,
@@ -28,16 +28,11 @@ return {
             },
             sections = {
                 lualine_a = {},
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = {
-                    {
-                        'filename',
-                        path = 1
-                    }
-                },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
+                lualine_b = {},
+                lualine_c = { 'branch', 'diff', 'diagnostics', { 'filename', path = 1 } },
+                lualine_x = { 'encoding', 'fileformat', 'filetype', 'progress', 'location' },
+                lualine_y = {},
+                lualine_z = {}
             },
             inactive_sections = {
                 lualine_a = {},
