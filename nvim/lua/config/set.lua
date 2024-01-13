f9b811eb0171da9762b01464e72eaaf629183b15
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-13 14:12:25 +0000

    fix win32 condition

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -42,7 +42,7 @@ if vim.g.vscode == nil then
 
     vim.opt.updatetime = 50
 
-    if vim.fn.has("win32") then
+    if vim.fn.has("win32") == 1 then
         vim.opt.shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell"
         vim.opt.shellcmdflag =
         "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
