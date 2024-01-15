db09e9cb403f9774c78136591e845f6a11b09551
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-15 17:23:42 +0000

    ignore case for flash jump

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -7,11 +7,24 @@ return {
         }
     },
     keys = {
-        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-        { "S",     mode = { "n", "o" },      function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+        {
+            "s",
+            mode = { "n", "x", "o" },
+            function()
+                local gi = vim.go.ignorecase
+                local gs = vim.go.smartcase
+                vim.go.ignorecase = true
+                vim.go.smartcase = false
+                require("flash").jump()
+                vim.go.ignorecase = gi
+                vim.go.smartcase = gs
+            end,
+            desc = "Flash"
+        },
+        { "S",     mode = { "n", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+        { "r",     mode = "o",          function() require("flash").remote() end,            desc = "Remote Flash" },
+        { "R",     mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" },      function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
     },
 }
 
