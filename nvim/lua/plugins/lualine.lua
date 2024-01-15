309e469569b0fcdf5ed704267f7324fccd43d829
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-15 09:26:17 +0000

    rename extension

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -60,7 +60,7 @@ return {
             tabline = {},
             winbar = {},
             inactive_winbar = {},
-            extensions = { 'ToggleTerm' }
+            extensions = { 'toggleterm' }
         }
     end
 }
