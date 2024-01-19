a88471d244eb054212bc7b52f7c36ebf5f364c51
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-19 18:36:03 +0000

    lazy load bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,7 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	version = "*",
+	event = "VeryLazy",
 	enabled = is_neovim,
 	config = function()
 		local bufferline = require("bufferline")
