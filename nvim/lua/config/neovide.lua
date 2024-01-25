00085550c71e25b2d55629fc7d9b405748ea4f1c
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-25 17:16:32 +0000

    reduce undercurl stroke scale

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -2,6 +2,7 @@ if vim.g.neovide then
 	vim.opt.guifont = { "JetBrainsMono NF", ":h11" }
 	vim.opt.linespace = 3
 	vim.g.neovide_scale_factor = 1.0
+	vim.g.neovide_underline_stroke_scale = 0.5
 	vim.g.neovide_remember_window_size = true
 	vim.g.neovide_cursor_animation_length = 0
 	vim.g.neovide_refresh_rate = 120
