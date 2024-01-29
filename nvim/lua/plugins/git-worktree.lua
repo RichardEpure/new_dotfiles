d22a24917bd1806f837537347238fd2072e0c5a0
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-01-29 22:26:06 +0000

    update mappings & lazy load

--- nvim/lua/plugins/git-worktree.lua
+++ nvim/lua/plugins/git-worktree.lua
@@ -3,21 +3,23 @@ local is_enabled = require("config.utils").is_neovim
 return {
 	"ThePrimeagen/git-worktree.nvim",
 	enabled = is_enabled,
+	keys = {
+		-- <c-d> - deletes that worktree
+		-- <c-f> - toggles forcing of the next deletion
+		{ "<leader>gt", [[:Telescope git_worktree<CR>]], desc = "Find git worktree" },
+		{
+			"<leader>gT",
+			function()
+				require("telescope").extensions.git_worktree.create_git_worktree()
+			end,
+			desc = "Create git worktree",
+		},
+	},
 	config = function()
 		local gwt = require("git-worktree")
 		gwt.setup()
 
 		local telescope = require("telescope")
 		telescope.load_extension("git_worktree")
-
-		-- <c-d> - deletes that worktree
-		-- <c-f> - toggles forcing of the next deletion
-		vim.keymap.set("n", "<Leader>ft", [[:Telescope git_worktree<CR>]], { desc = "Find git worktree" })
-		vim.keymap.set(
-			"n",
-			"<Leader>gt",
-			telescope.extensions.git_worktree.create_git_worktree,
-			{ desc = "Create git worktree" }
-		)
 	end,
 }
