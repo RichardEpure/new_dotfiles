d8e4642b6b52903967a6e8da0b6549abf37a0f80
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-02-07 23:30:40 +0000

    fix clipboard performance in wsl

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -41,4 +41,19 @@ if vim.g.vscode == nil then
 	vim.opt.isfname:append("@-@")
 
 	vim.opt.updatetime = 50
+
+	if vim.fn.has("wsl") == 1 then
+		vim.g.clipboard = {
+			name = "WslClipboard",
+			copy = {
+				["+"] = "clip.exe",
+				["*"] = "clip.exe",
+			},
+			paste = {
+				["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+				["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			},
+			cache_enabled = 0,
+		}
+	end
 end
