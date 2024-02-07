14e10f42467fe8631b411122a3988c3485104748
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-02-07 21:33:22 +0000

    remove tabout

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -27,7 +27,6 @@ return {
 
 		-- Other
 		{ "folke/neodev.nvim" },
-		{ "abecodes/tabout.nvim" },
 	},
 	config = function()
 		local lsp_zero = require("lsp-zero")
@@ -217,28 +216,6 @@ return {
 					vim.api.nvim_command([[echo serverstart(']] .. pipe .. [[')]])
 				end
 			end,
-		})
-
-		-- Tabout
-		require("tabout").setup({
-			tabkey = "<C-Tab>", -- key to trigger tabout, set to an empty string to disable
-			backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
-			act_as_tab = false, -- shift content if tab out is not possible
-			act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-			default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-			default_shift_tab = "<C-d>", -- reverse shift default action,
-			enable_backwards = true, -- well ...
-			completion = true, -- if the tabkey is used in a completion pum
-			tabouts = {
-				{ open = "'", close = "'" },
-				{ open = '"', close = '"' },
-				{ open = "`", close = "`" },
-				{ open = "(", close = ")" },
-				{ open = "[", close = "]" },
-				{ open = "{", close = "}" },
-			},
-			ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-			exclude = {}, -- tabout will ignore these filetypes
 		})
 	end,
 }
