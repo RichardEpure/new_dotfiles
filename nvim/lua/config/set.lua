c158efe08802aef2731b435c76f7cc810a610ed7
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-02-08 16:57:35 +0000

    use win32yank to fix hanging on focus

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -44,16 +44,16 @@ if vim.g.vscode == nil then
 
 	if vim.fn.has("wsl") == 1 then
 		vim.g.clipboard = {
-			name = "WslClipboard",
+			name = "win32yank_nvim",
 			copy = {
-				["+"] = "clip.exe",
-				["*"] = "clip.exe",
+				["+"] = "win32yank.exe -i --crlf",
+				["*"] = "win32yank.exe -i --crlf",
 			},
 			paste = {
-				["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-				["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+				["+"] = "win32yank.exe -o --lf",
+				["*"] = "win32yank.exe -o --lf",
 			},
-			cache_enabled = 0,
+			cache_enabled = 1,
 		}
 	end
 end
