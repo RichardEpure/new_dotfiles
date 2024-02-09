4d081d11d2b08db38c7266d66282ca56a937cdf7
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-02-09 20:02:11 +0000

    faster method of detecting if inside a git repo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -79,19 +79,16 @@ return {
 
 		local builtin = require("telescope.builtin")
 
-		-- We cache the results of "git rev-parse"
-		-- Process creation is expensive in Windows, so this reduces latency
-		local is_inside_work_tree = {}
+		local is_git_repo = {}
 		local project_files = function()
-			local opts = {} -- define here if you want to define something
+			local opts = {}
 
 			local cwd = vim.fn.getcwd()
-			if is_inside_work_tree[cwd] == nil then
-				vim.fn.system("git rev-parse --is-inside-work-tree")
-				is_inside_work_tree[cwd] = vim.v.shell_error == 0
+			if is_git_repo[cwd] == nil then
+				is_git_repo[cwd] = vim.fn.glob(".git") ~= ""
 			end
 
-			if is_inside_work_tree[cwd] then
+			if is_git_repo[cwd] then
 				builtin.git_files(opts)
 			else
 				builtin.find_files(opts)
