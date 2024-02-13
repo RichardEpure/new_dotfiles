cdee386dc9debc931e83cd1f79ab659eb9db6cb5
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-02-13 23:43:42 +0000

    lazy load harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -5,41 +5,79 @@ return {
 	enabled = is_neovim,
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon.setup({})
+	keys = {
+		{
+			"<leader>a",
+			function()
+				require("harpoon"):list():append()
+			end,
+			desc = "Add to Harpoon list",
+		},
+		{
+			"<leader>sf",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "Toggle Harpoon list",
+		},
+		{
+			"<leader>sh",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+			desc = "Select Harpoon list 1",
+		},
+		{
+			"<leader>sj",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+			desc = "Select Harpoon list 2",
+		},
+		{
+			"<leader>sk",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+			desc = "Select Harpoon list 3",
+		},
+		{
+			"<leader>sl",
+			function()
+				require("harpoon"):list():select(4)
+			end,
+			desc = "Select Harpoon list 4",
+		},
+		{
+			"<leader>s;",
+			function()
+				require("harpoon"):list():select(5)
+			end,
+			desc = "Select Harpoon list 5",
+		},
+		{
+			"<leader>s'",
+			function()
+				require("harpoon"):list():select(6)
+			end,
+			desc = "Select Harpoon list 6",
+		},
 
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
-		end, { desc = "Add to Harpoon list" })
-		vim.keymap.set("n", "<leader>sf", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end, { desc = "Toggle Harpoon list" })
-
-		vim.keymap.set("n", "<leader>sh", function()
-			harpoon:list():select(1)
-		end, { desc = "Select Harpoon list 1" })
-		vim.keymap.set("n", "<leader>sj", function()
-			harpoon:list():select(2)
-		end, { desc = "Select Harpoon list 2" })
-		vim.keymap.set("n", "<leader>sk", function()
-			harpoon:list():select(3)
-		end, { desc = "Select Harpoon list 3" })
-		vim.keymap.set("n", "<leader>sl", function()
-			harpoon:list():select(4)
-		end, { desc = "Select Harpoon list 4" })
-		vim.keymap.set("n", "<leader>s;", function()
-			harpoon:list():select(5)
-		end, { desc = "Select Harpoon list 5" })
-		vim.keymap.set("n", "<leader>s'", function()
-			harpoon:list():select(6)
-		end, { desc = "Select Harpoon list 6" })
-
-		vim.keymap.set("n", "<leader>sp", function()
-			harpoon:list():prev()
-		end, { desc = "Select previous in Harpoon list" })
-		vim.keymap.set("n", "<leader>sn", function()
-			harpoon:list():next()
-		end, { desc = "Select next in Harpoon list" })
-	end,
+		{
+			"<leader>sp",
+			function()
+				require("harpoon"):list():prev()
+			end,
+			desc = "Select previous in Harpoon list",
+		},
+		{
+			"<leader>sn",
+			function()
+				require("harpoon"):list():next()
+			end,
+			desc = "Select next in Harpoon list",
+		},
+	},
+	opts = {},
 }
