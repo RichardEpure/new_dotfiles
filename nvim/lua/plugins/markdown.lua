bbc8e35b28fb80a9e378280bc40d259fbdd871d6
Richard <43828146+RichardEpure@users.noreply.github.com>
2024-06-09 15:26:05 +0100

    add markdown preview plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,13 +1,24 @@
 local is_neovim = require("config.utils").is_neovim
 
 return {
-	"MeanderingProgrammer/markdown.nvim",
-	name = "render-markdown",
-	ft = { "markdown" },
-	cmd = { "RenderMarkdownToggle" },
-	enabled = is_neovim,
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	config = function()
-		require("render-markdown").setup({})
-	end,
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		name = "render-markdown",
+		cmd = { "RenderMarkdownToggle" },
+		ft = { "markdown" },
+		enabled = is_neovim,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("render-markdown").setup({})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
