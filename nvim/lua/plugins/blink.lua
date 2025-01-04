643b6a124b3078c738a5c425c3120bfaefe13c90
Richard <43828146+RichardEpure@users.noreply.github.com>
2025-01-04 01:06:25 +0000

    disable signatures

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -40,9 +40,10 @@ return {
 			},
 		},
 
-		signature = {
-			enabled = true,
-		},
+		-- Too laggy sometimes
+		-- signature = {
+		-- 	enabled = true,
+		-- },
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
