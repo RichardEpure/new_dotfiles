08019b98c943e2a31e61c71205d19cdb5ec8c6db
Richard <43828146+RichardEpure@users.noreply.github.com>
2026-09-20 20:36:42 +0100

    Fix wrapping peristing to non-wrapping filetypes

--- nvim/lua/config/filetype.lua
+++ nvim/lua/config/filetype.lua
@@ -5,3 +5,19 @@ vim.filetype.add({
 		j2 = "jinja",
 	},
 })
+
+local wrapping_filetypes = {
+	markdown = true,
+	typst = true,
+}
+
+-- Wrapping is window-local, so reapply it when a window displays another buffer.
+vim.api.nvim_create_autocmd({ "FileType", "BufWinEnter", "WinEnter" }, {
+	group = vim.api.nvim_create_augroup("filetype_wrapping", { clear = true }),
+	desc = "Wrap only prose filetypes",
+	callback = function()
+		local wrap = wrapping_filetypes[vim.bo.filetype] == true
+		vim.wo.wrap = wrap
+		vim.wo.linebreak = wrap
+	end,
+})
